f06e9bd7631d8274314910d8b786743f768610d4
Josh Jaquez <jjaquez@wisc.edu>
2018-11-11 09:14:47 -0800

    edit package.json and server.js to prepare for heroku development

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const recipes = require('./routes/api/recipes');
 const user = require('./routes/api/user');
 const app = express();
-
+const path = require('path');
 const db = require('./config/keys').mongoURI;
 //Body Parser Middleware
 app.use(bodyParser.urlencoded({extended: false}));
@@ -18,6 +18,15 @@ mongoose
 
 app.use('/api/recipes', recipes);
 app.use('/api/user', user);
+
+//serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
